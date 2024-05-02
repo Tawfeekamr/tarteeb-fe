8a7317dc0566fa96612c6dd2a45cf652078250ff
tawfeekamr <tawfeekamr91@gamil.com>
2024-05-02 22:00:43 +0300

    fix cors

--- next.config.mjs
+++ next.config.mjs
@@ -1,20 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    async headers() {
-        return [
-            {
-                source: "/api/(.*)", // Matches all API routes
-                headers: [
-                    { key: "Access-Control-Allow-Credentials", value: "true" },
-                    { key: "Access-Control-Allow-Origin", value: "*" },
-                    { key: "Access-Control-Allow-Origin", value: "https://tarteeb-cms.payloadcms.app/api" },
-                    { key: "Access-Control-Allow-Origin", value: "https://tarteeb-cms.payloadcms.app" }, // Replace with the actual origin you want to allow
-                    { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,POST,PUT,DELETE" }, // Allowed HTTP methods (adjust as needed)
-                    { key: "Access-Control-Allow-Headers", value: "Content-Type, Authorization" }, // Allowed headers (adjust as needed)
-                ],
-            },
-        ];
-    },
+
 };
 
 export default nextConfig;
