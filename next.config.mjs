a7fbde2789d93b8f384ca527a09fa0daab3728cf
tawfeekamr <tawfeekamr91@gamil.com>
2024-05-02 21:36:24 +0300

    Add fixes home page

--- next.config.mjs
+++ next.config.mjs
@@ -7,6 +7,7 @@ const nextConfig = {
                 headers: [
                     { key: "Access-Control-Allow-Credentials", value: "true" },
                     { key: "Access-Control-Allow-Origin", value: "*" },
+                    { key: "Access-Control-Allow-Origin", value: "https://tarteeb-cms.payloadcms.app/api" },
                     { key: "Access-Control-Allow-Origin", value: "https://tarteeb-cms.payloadcms.app" }, // Replace with the actual origin you want to allow
                     { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,POST,PUT,DELETE" }, // Allowed HTTP methods (adjust as needed)
                     { key: "Access-Control-Allow-Headers", value: "Content-Type, Authorization" }, // Allowed headers (adjust as needed)
